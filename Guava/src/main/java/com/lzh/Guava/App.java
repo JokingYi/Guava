72052663f75b32fe0238775df0d892834d5b3589
JokingYi <leviosa_lzh@163.com>
2017-06-14 14:20:19 +0800

    init commit

---
+++ Guava/src/main/java/com/lzh/Guava/App.java
@@ -0,0 +1,13 @@
+package com.lzh.Guava;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
